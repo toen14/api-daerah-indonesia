c468499a5faacc5892b9ec29a2cd45951c2b7050
La Atun <w3r3n3@gmail.com>
2021-11-07 12:52:23 +0800

    atasi masalah status code 304

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const app = require('express')();
 const errorHandler = require('./middlewares/error-handler');
 const CustomError = require('./errors/custom-error');
 
-const port = process.env.PORT || 1945;
+const port = process.env.PORT || 19451;
 const db = require('./db/db.config');
+
+// Atasi status code selalu 304
+app.disable('etag');
 
 // Tampilan default API
 app.get('/', (_, res) => {
